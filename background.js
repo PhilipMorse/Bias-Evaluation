fe24ff23a048885e88566dd935a0da28dd730eac
Philip Morse <philipmorse1234@gmail.com>
2019-09-08 21:27:27 -0400

    First Working Version

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
+	chrome.tabs.executeScript(null, {
 		file: 'inject.js'
 	});
 });
